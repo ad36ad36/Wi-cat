1bc17e39ab2868975fd4a23690ae0d99e101d59e
ad36ad36 <53022215+ad36ad36@users.noreply.github.com>
2019-07-29 15:22:56 -0700

    Update schedule_input_UPDATED.ino

--- schedule_input_UPDATED.ino
+++ schedule_input_UPDATED.ino
@@ -12,9 +12,9 @@ bool change; //boolean to check if a user wants to change schedule
 int indexx = 1; //indexx for different times of the day
 
 // set up all feeds
-AdafruitIO_Feed *button = io.feed("button");
-AdafruitIO_Feed *test = io.feed("test");
-AdafruitIO_Feed *text = io.feed("text");
+AdafruitIO_Feed *button = io.feed("button"); //momentary button   
+AdafruitIO_Feed *test = io.feed("test"); // display 
+AdafruitIO_Feed *text = io.feed("text"); // user input
 
 void setup() {
   // start the serial connection
