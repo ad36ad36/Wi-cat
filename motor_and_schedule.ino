e71309643590b8cad83777ca0ab8de0e653ff42f
wdanny69 <39199279+wdanny69@users.noreply.github.com>
2019-07-30 12:51:25 -0700

    Update motor_and_schedule.ino

--- motor_and_schedule.ino
+++ motor_and_schedule.ino
@@ -1,31 +1,58 @@
-//This code was written by Danny W, Ranvir, Adrian, and Todd Treece for Adafruit Industries
-
-
 #include <Stepper.h>
 
+//This code was written by Danny W., Ranvir, Adrian, 
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
 
+
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME   "inukealot"
+#define IO_KEY        "0d8142ca7e074d50adf5781f991ea0fd"
+
+#define WIFI_SSID   "dw123"
+#define WIFI_PASS   "1998meborn"
+
+#include "AdafruitIO_WiFi.h"
+
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
+  // Configure the pins used for the ESP32 connection
+  #if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+    // Don't change the names of these #define's! they match the variant ones
+    #define SPIWIFI SPI
+    #define SPIWIFI_SS 10  // Chip select pin
+    #define SPIWIFI_ACK 9  // a.k.a BUSY or READY pin
+    #define ESP32_RESETN 6 // Reset pin
+    #define ESP32_GPIO0 -1 // Not connected
+  #endif
+  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS, SPIWIFI_ACK, ESP32_RESETN, ESP32_GPIO0, &SPIWIFI);
+#else
+  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
+
+//initialize all variables
 int hours1;
 int minutes1;
 int hours2;
 int minutes2;
-
 bool change; //boolean to check if a user wants to change schedule
 int indexx = 1; //indexx for different times of the day
-
 const int   MAX_SERVING = 200; // make max serving corresponsd to the size of average bowl?
+const int stepsPerRevolution = 48;  //specification of motor (found by 360/(stride angle of motor)
+bool toggle = false; //checks whether the device is "on" or "off"
 
-const int stepsPerRevolution = 48;  // change this to fit the number of steps per revolution
-// for your motor
+Stepper myStepper(stepsPerRevolution, 14, 15, 32, 33); //set up stepper motor
 
-bool toggle = false;
-
-Stepper myStepper(stepsPerRevolution, 14, 15, 32, 33); 
-
+//set up all feeds for Adafruit IO
 AdafruitIO_Feed *serving_size_feed = io.feed("serving-size"); 
 AdafruitIO_Feed *toggle_feed = io.feed("toggle");
 AdafruitIO_Feed *schedule_reset_feed = io.feed("schedule-reset");
 AdafruitIO_Feed *schedule_input_feed = io.feed("schedule-input");
 AdafruitIO_Feed *display_feed = io.feed("display");
+
 
 void setup() {
    myStepper.setSpeed(10);  // set the speed at 60 rpm:
@@ -39,9 +66,9 @@ void setup() {
 
   io.connect(); // connect to io.adafruit.com
 
-
+  //runs functions based on what feeds receive data
   schedule_reset_feed->onMessage(changeSchedule);
-  schedule_input_feed->onMessage(handleschedule_input_feed);
+  schedule_input_feed->onMessage(handleInput);
   toggle_feed->onMessage(toggleControl);
   serving_size_feed->onMessage(StepperControl);
 
@@ -57,12 +84,15 @@ void setup() {
   serving_size_feed->get();
 }
 
+
 void loop() {
-//  Serial.println(sum);
-//  delay(500);
-  io.run();
+  
+  io.run(); //checks io for any incoming data
+
+  //checks if user wants to change schedule
   if (change == true) {
     display_feed->save("What time to feed in the morning?");
+    //waits for user to provide an input through the text block on the IO
     while (change == true) {
       io.run();
       Serial.println(".");
@@ -70,36 +100,37 @@ void loop() {
         break;
       }
     }
-  display_feed->save("saved");
-delay(1000);
-  //asks for input for evening schedule
-  indexx++;
-  display_feed->save("What time to feed in the evening?");
-  change = true;
-  while ( change == true ) {
-    io.run();
-    Serial.println(".");
-    if (change == false)
-      break;
+    display_feed->save("saved");
+    delay(500);
+    
+    //asks for input for evening schedule
+    indexx++;
+    display_feed->save("What time to feed in the evening?");
+    change = true;
+    while ( change == true ) {
+      io.run();
+      Serial.println(".");
+      if (change == false)
+        break;
+    }
+    delay(500);
+    display_feed->save("saved");
   }
-  delay(500);
-  display_feed->save("saved");
-  }
-  
+
 }
 
-//checks if user has pressed schedule_reset_feed indicating a change in schedule
+
+//checks if user has pressed the button indicating a change in schedule
 void changeSchedule (AdafruitIO_Data *data) {
-  
   change = true;
 }
+
 
 //function to handle schedule_input_feed input and stores values
-void handleschedule_input_feed (AdafruitIO_Data *data) {
+void handleInput (AdafruitIO_Data *data) {
   if (change == true) { //makes sure if any accidental input from schedule_input_feed box doesn't change schedule
     //converts data into a string
     String schedule_input_feed1 = data->toString();
-    
     
     //convert string into char array
     char schedule_input_feed2[schedule_input_feed1.length()+1];
@@ -112,7 +143,8 @@ void handleschedule_input_feed (AdafruitIO_Data *data) {
     strings[1] = ptr;
     ptr = strtok(NULL , "");
     strings[2] = ptr;
-  
+
+    
     //convert char array into integers
     //stores time for morning schedule
     if (indexx == 1) {
@@ -128,10 +160,12 @@ void handleschedule_input_feed (AdafruitIO_Data *data) {
       Serial.println(hours2);
       Serial.println(minutes2);
     }
+    if ( (hours1
     change = false;
   }
   
 }
+
 
 void StepperControl(AdafruitIO_Data *data) {
   
@@ -148,7 +182,8 @@ void StepperControl(AdafruitIO_Data *data) {
 
 }
 
-//https://www.arduino.cc/reference/en/language/structure/sketch/loop/
+
+//ON/OFF function of device ; determine whether device will run
 void toggleControl(AdafruitIO_Data *data) {  
   
   // handle the toggle on the Adafruit IO Dashboard
