da5d32bd96b493af74a39c29381ca5763fe48236
ad36ad36 <53022215+ad36ad36@users.noreply.github.com>
2019-07-30 14:12:27 -0700

    Update motor_and_schedule.ino

--- motor_and_schedule.ino
+++ motor_and_schedule.ino
@@ -1,37 +1,11 @@
 #include <Stepper.h>
+#include "time.h"
+#include "config.h"
 
 //This code was written by Danny W., Ranvir, Adrian, 
 // Written by Todd Treece for Adafruit Industries
 // Copyright (c) 2016 Adafruit Industries
 // Licensed under the MIT license.
-
-
-/************************ Adafruit IO Config *******************************/
-
-// visit io.adafruit.com if you need to create an account,
-// or if you need your Adafruit IO key.
-#define IO_USERNAME   "inukealot"
-#define IO_KEY        "0d8142ca7e074d50adf5781f991ea0fd"
-
-#define WIFI_SSID   "dw123"
-#define WIFI_PASS   "1998meborn"
-
-#include "AdafruitIO_WiFi.h"
-
-#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
-  // Configure the pins used for the ESP32 connection
-  #if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
-    // Don't change the names of these #define's! they match the variant ones
-    #define SPIWIFI SPI
-    #define SPIWIFI_SS 10  // Chip select pin
-    #define SPIWIFI_ACK 9  // a.k.a BUSY or READY pin
-    #define ESP32_RESETN 6 // Reset pin
-    #define ESP32_GPIO0 -1 // Not connected
-  #endif
-  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS, SPIWIFI_ACK, ESP32_RESETN, ESP32_GPIO0, &SPIWIFI);
-#else
-  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
-#endif
 
 //initialize all variables
 int hours1;
